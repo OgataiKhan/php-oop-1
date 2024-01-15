58e3a2c95df0c5d0b83a828c3c8af6101ebcb9e0
OgataiKhan <badellino.kevin@gmail.com>
2024-01-15 18:27:28 +0100

    feat: add constructor

--- index.php
+++ index.php
@@ -10,18 +10,38 @@ class Movie
   public $director;
   public $genre;
   public $language;
+  
+  /**
+   * __construct
+   *
+   * @return void
+   */
+  function __construct(
+    $_title,
+    $_director,
+    $_genre,
+    $_language
+  ) {
+    $this->title = $_title;
+    $this->director = $_director;
+    $this->genre = $_genre;
+    $this->language = $_language;
+  }
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP OOP 1</title>
 </head>
+
 <body>
-  
+
 </body>
+
 </html>
