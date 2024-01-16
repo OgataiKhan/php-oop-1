881e8b2a45ef753d6a0895a2a1c31d1d6d3bc5f9
OgataiKhan <badellino.kevin@gmail.com>
2024-01-16 10:32:18 +0100

    fix: remove superfluous divs

--- _partials/main.php
+++ _partials/main.php
@@ -3,9 +3,7 @@
     <ul class="d-flex justify-content-center flex-wrap">
       <?php
       foreach ($movies as $movie) {
-        echo "<div>";
         $movie->displayProperties();
-        echo "</div>";
       }
       ?>
     </ul>
