5e787209b6c85b3356f33cdc29d64b368591966d
OgataiKhan <badellino.kevin@gmail.com>
2024-01-16 10:40:53 +0100

    refactor: simplify db

--- src/db.php
+++ src/db.php
@@ -1,9 +1,8 @@
 <?php
 
 // Creating movie instances
-$rebel_moon = new Movie('Rebel Moon', 2023, 'Zack Snyder', ['Science Fiction', 'Action', 'Adventure', 'Drama'], 'English');
-$wonka = new Movie('Wonka', 2023, 'Paul King', ['Fantasy', 'Musical', 'Comedy', 'Adventure'], 'English');
-$avatar3 = new Movie('Avatar 3', 2025, 'James Cameron', ['Science Fiction', 'Action', 'Adventure', 'Drama'], 'English');
-
-// Storing movies in an array
-$movies = array($rebel_moon, $wonka, $avatar3);
+$movies = [
+  new Movie('Rebel Moon', 2023, 'Zack Snyder', ['Science Fiction', 'Action', 'Adventure', 'Drama'], 'English'),
+  new Movie('Wonka', 2023, 'Paul King', ['Fantasy', 'Musical', 'Comedy', 'Adventure'], 'English'),
+  new Movie('Avatar 3', 2025, 'James Cameron', ['Science Fiction', 'Action', 'Adventure', 'Drama'], 'English'),
+];
