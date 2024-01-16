ede5fbdaa4659dae3e3314b51a84aaaad49a8c9d
OgataiKhan <badellino.kevin@gmail.com>
2024-01-16 07:25:59 +0100

    fix: order genres

--- src/db.php
+++ src/db.php
@@ -3,7 +3,7 @@
 // Creating movie instances
 $rebel_moon = new Movie('Rebel Moon', 'Zack Snyder', ['Science Fiction', 'Action', 'Adventure', 'Drama'], 'English');
 $wonka = new Movie('Wonka', 'Paul King', ['Fantasy', 'Musical', 'Comedy', 'Adventure'], 'English');
-$avatar3 = new Movie('Avatar 3', 'James Cameron', ['Science Fiction', 'Action', 'Drama', 'Adventure'], 'English');
+$avatar3 = new Movie('Avatar 3', 'James Cameron', ['Science Fiction', 'Action', 'Adventure', 'Drama'], 'English');
 
 // Setting release years
 $rebel_moon->setYear(2023);
