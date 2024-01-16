ea7f6ed519bbe5bb9dec87c9564f5a65f67184a2
OgataiKhan <badellino.kevin@gmail.com>
2024-01-16 07:27:40 +0100

    fix: remove commented code

--- models/Movie.php
+++ models/Movie.php
@@ -48,27 +48,16 @@ class Movie
    *
    * @return void
    */
-  /*
-  public function displayProperties()
-  {
-    echo "<h2>$this->title</h2>";
-    echo "<p>Release Year: $this->year</p>";
-    echo "<p>Director: $this->director</p>";
-    echo "<p>Genre: " . implode(", ", $this->genre) . "</p>";
-    echo "<p>Language: $this->language</p>";
-  }
-  */
-
   public function displayProperties()
   {
     echo "<li class=\"card shadow m-2\" style=\"width: 18rem;\">
-    <div class=\"card-body\">
+      <div class=\"card-body\">
       <h5 class=\"card-title\">$this->title</h5>
       <h6 class=\"card-subtitle mb-2 text-body-secondary\">$this->year</h6>
       <p class=\"card-text\"><span class=\"fw-bold\">Director:</span> $this->director</p>
       <p class=\"card-text\"><span class=\"fw-bold\">Genre:</span> " . implode(", ", $this->genre) . "</p>
       <p class=\"card-text\"><span class=\"fw-bold\">Language:</span> $this->language</p>
-    </div>
-  </li>";
+      </div>
+    </li>";
   }
 }
