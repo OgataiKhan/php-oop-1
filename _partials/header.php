d33cf70b300ac77404d0fccd560c90cf0f287d12
OgataiKhan <badellino.kevin@gmail.com>
2024-01-16 06:47:15 +0100

    feat: bootstrap & header

---
+++ _partials/header.php
@@ -0,0 +1,5 @@
+<header class="py-3">
+  <div class="container">
+    <h1>Movie Catalogue</h1>
+  </div>
+</header>
--- index.php
+++ index.php
@@ -10,9 +10,18 @@ require_once __DIR__ . '/src/db.php';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP OOP 1</title>
+  <!-- Bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <!-- /Bootstrap -->
+  <!-- My css -->
+  <link rel="stylesheet" href="src/css/style.css">
+  <!-- /My css -->
 </head>
 
 <body>
+  <?php
+  include __DIR__ . '/_partials/header.php';
+  ?>
   <?php
   foreach ($movies as $movie) {
     echo "<div>";
